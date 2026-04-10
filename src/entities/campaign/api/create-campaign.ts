47235d93fc47f167274c39f8adef08ca59536fa5
Einere <kjwsx23@gmail.com>
2026-04-11 00:44:37 +0900

    chore: 타당성 불합 todo 코멘트 제거

--- src/entities/campaign/api/create-campaign.ts
+++ src/entities/campaign/api/create-campaign.ts
@@ -1,7 +1,6 @@
 import type { CreateCampaignInput } from "@/entities/campaign/lib/create-campaign-schema";
 import type { Campaign } from "@/entities/campaign/model/types";
 
-/*TODO: shared/api 로 이동해야 하는게 맞는거 아닌가? */
 export async function createCampaign(
 	input: CreateCampaignInput,
 ): Promise<Campaign> {
